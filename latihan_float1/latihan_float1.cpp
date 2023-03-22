9aa9ea3b7e50c16eacdabd1b3190acfbbd6052a6
agilimam <agilimamalhafizi04@gmail.com>
2023-03-22 17:10:06 +0700

    float luas

---
+++ latihan_float1/latihan_float1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+float luaspersegipanjang(float p, float l) {
+	return p * l;
+}
+
+float luaslingkaran(float r) {
+	return 3.14 * r * r;
+}
+
+float luassegitiga(float a, float t) {
+	return 0, 5 * a * t;
+}
